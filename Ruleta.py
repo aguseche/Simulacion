147949d9c582eb719dcec985549209c5c04c6618
eche <etxeagus@gmail.com>
2020-03-26 23:07:42 -0300

    solo ruleta

--- Ruleta.py
+++ Ruleta.py
@@ -1,1 +1,22 @@
-#asd
+#Como ejercicio tenemos que hacer una ruleta y hacer las graficas de ...
+import random as ran
+
+def mensaje():
+    print ("Bienvenido a la ruleta!!")
+
+
+def ruleta():
+    n = ran.randint(0, 36)
+    return n
+
+def main():
+    mensaje()
+    op = "S"
+    while op == "S":
+        num = ruleta()
+        print(num)
+        op = input("Ingrese S para conseguir otro numero: ").upper()
+    print("Finalizado.")
+
+
+main()
