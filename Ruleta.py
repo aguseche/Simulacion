020562dfb2973699c442c85ec98b660d3b4d3505
Agustin Etcheverry <etxeagus@gmail.com>
2020-04-08 10:33:47 -0300

    Hasta media de media

--- Ruleta.py
+++ Ruleta.py
@@ -1,37 +1,97 @@
-#Como ejercicio tenemos que hacer una ruleta y hacer las graficas de ...
-#import random as ran
+# Como ejercicio tenemos que hacer una ruleta y hacer las graficas
+# 1- fr/n frecuencia relativa #cantidad de veces que SALIO el numero / cantidad total de muestras
+# 2- vp/n valor promedio de las tiradas
+# 3- vd/n valor del desvio       np.std(datos, 0) # Desviación típica de cada columna
+# 4- vv/n valor de la varianza
 import numpy as np
-import matplotlib as matp
+import matplotlib.pyplot as plt
+import random as rn
+import matplotlib.ticker as tick
+
+n_muestras = 1000
+min_n = 0
+max_n = 36
+
+data = []
+media = []
+media_media = []
+varianza = []
+varianza_media = []
 
 
-def mensaje():
-    print ("Bienvenido a la ruleta!!")
+def cambiar_y(x, pos):
+    return x / len(data)
+
+def plot_frAbs(d):  # graficar histograma de frecuencias absolutas
+    fig, ax = plt.subplots()
+    ax.hist(d, bins=max_n + 1)
+    fig.tight_layout()
+    ax.set_xlabel('Numeros de la ruleta')
+    ax.set_ylabel('Cantidad')
+    plt.title("Histograma de frecuencia absoluta")
+    plt.show()
 
 
-def ruleta():
-    n = np.randint(0, 36)
-    return n
+def plot_frRel(d):  # graficar histograma de frecuencias relativas
+    fig, ax = plt.subplots()
+    ax.hist(d, bins=max_n + 1)
+    ax.yaxis.set_major_formatter(tick.FuncFormatter(cambiar_y))  # para cambiar los valores mostrados en y
+    fig.tight_layout()
+    ax.set_xlabel('Numeros de la ruleta')
+    ax.set_ylabel('Cantidad / 100')
+    plt.title("Histograma de frecuencia relativa")
+    plt.show()
 
 
-def opciones():
-    numeros = []
-    op = "S"
-    while op == "S":
-        num = ruleta()
-        numeros.append(num)
-        print(num)
-        op = input("Ingrese S para conseguir otro numero: ").upper()
-    print("Finalizado.")
-    return numeros
+def plot_media(m):  # graficar histograma de media
+    fig, ax = plt.subplots()
+    plt.plot(list(range(len(m))), m, color='k')
+    ax.set_xlabel('Cantidad de tiradas')
+    ax.set_ylabel('Media')
+    plt.title("Histograma de media")
+    plt.show()
 
-def Calculo(lis):
-    pass
+
+def plot_media_media(mm):   # graficar histograma de media de la media
+    fig, ax = plt.subplots()
+    plt.plot(list(range(len(mm))), mm, color='k')
+    ax.set_xlabel('Cantidad de tiradas')
+    ax.set_ylabel('Media de la media')
+    plt.title("Histograma de media de la media")
+    plt.show()
+
+def plot_mymm(m, mm):
+    fig, ax = plt.subplots()
+    plt.plot(list(range(len(m))), m, color='k')
+    plt.plot(list(range(len(mm))), mm, color='white')
+    ax.set_xlabel('Cantidad de tiradas')
+    ax.set_ylabel('Medias')
+    plt.title("Histograma de media de la media")
+    plt.show()
+
+
+def plot(d, m, mm):
+    plot_frAbs(d)
+    plot_frRel(d)
+    plot_media(m)
+    plot_media_media(mm)
+    plot_mymm(m, mm)
+
+
+def mostrar_datos(d):
+    for i in range(min_n, max_n + 1):
+        print(str(i) + ": " + str(d.count(i)))
 
 
 def main():
-    mensaje()
-    lista = opciones()
-    print(lista)
+    for i in range(n_muestras):
+        data.append(rn.randint(min_n, max_n))
+        media.append(np.mean(data))
+        media_media.append(np.mean(media))
+        # if i >=2:
+
+    plot(data, media, media_media)
+    mostrar_datos(data)
 
 
 main()
--- Tp-1.1.py
+++ Tp-1.1.py
@@ -1,31 +1,72 @@
-#Como ejercicio tenemos que hacer una ruleta y hacer las graficas
-#1- fr/n frecuencia relativa #cantidad de veces que SALIO el numero / cantidad total de muestras
-#2- vp/n valor promedio de las tiradas
-#3- vd/n valor del desvio       np.std(datos, 0) # Desviación típica de cada columna
-#4- vv/n valor de la varianza
+# Como ejercicio tenemos que hacer una ruleta y hacer las graficas
+# 1- fr/n frecuencia relativa #cantidad de veces que SALIO el numero / cantidad total de muestras
+# 2- vp/n valor promedio de las tiradas
+# 3- vd/n valor del desvio       np.std(datos, 0) # Desviación típica de cada columna
+# 4- vv/n valor de la varianza
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+import random as rn
+import matplotlib.ticker as tick
+
+n_muestras = 1000
+min_n = 0
+max_n = 36
+
+data = []
+media = []
+media_media = []
+varianza = []
+varianza_media = []
 
 
-def mensaje():
-    print ("Bienvenido a la ruleta!!")
+def cambiar_y(x, pos):
+    return x / len(data)
 
 
-def ruleta(n):
-    return np.random.randint(0, 36, n)
+def plot(d, m, mm):
+    # graficar histograma de frecuencias absolutas
+
+    fig, ax = plt.subplots()
+    ax.hist(d, bins=max_n + 1)
+    fig.tight_layout()
+    ax.set_xlabel('Numeros de la ruleta')
+    ax.set_ylabel('Cantidad')
+    plt.title("Histograma de frecuencia absoluta")
+    plt.show()
+
+    # graficar histograma de frecuencias relativas
+    fig, ax = plt.subplots()
+    ax.hist(d, bins=max_n + 1)
+    ax.yaxis.set_major_formatter(tick.FuncFormatter(cambiar_y))  # para cambiar los valores mostrados en y
+    fig.tight_layout()
+    ax.set_xlabel('Numeros de la ruleta')
+    ax.set_ylabel('Cantidad / 100')
+    plt.title("Histograma de frecuencia absoluta")
+    plt.show()
+
+    # graficar histograma de media
+    fig, ax = plt.subplots()
+    plt.plot(list(range(len(m))), m, color='k')
+    ax.set_xlabel('Cantidad de tiradas')
+    ax.set_ylabel('Media')
+    plt.title("Histograma de media")
+    plt.show()
 
 
-def AnalizarDatos(datos):
-    a = pd.value_counts(datos) #encontras la frecuencia absoluta de cada numero
-    dataframe = pd.DataFrame(a, columns=["FrAbs"])
-    print(dataframe)
+def mostrar_datos(d):
+    for i in range(min_n, max_n + 1):
+        print(str(i) + ": " + str(d.count(i)))
 
 
 def main():
-    mensaje()
-    d = ruleta(int(input("Ingrese la cantidad de muestras que desea: ")))
-    #print(d)
-    AnalizarDatos(d)
+    for i in range(n_muestras):
+        data.append(rn.randint(min_n, max_n))
+        media.append(np.mean(data))
+        media_media.append(np.mean(media))
+        # if i >=2:
+
+    plot(data, media, media_media)
+    mostrar_datos(data)
 
 
 main()
